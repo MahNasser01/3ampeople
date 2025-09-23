5c63e36517c56fb0f5148110a3bda6b6e30cb5e6
Ahmed Gomaa <ahmedgomaa8998@gmail.com>
2025-09-23 17:39:12 +0300

    feat/add get applications endpoint

---
+++ src/app/api/get-applications/route.ts
@@ -0,0 +1,27 @@
+import { nanoid } from "nanoid";
+import { NextResponse } from "next/server";
+import { InterviewService } from "@/services/interviews.service";
+import { logger } from "@/lib/logger";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export async function GET(req: Request, res: Response) {
+  try {
+    const supabase = createClientComponentClient();
+    const { data, error } = await supabase
+      .from("interview")
+      .select("*")
+      .order("created_at", { ascending: false });
+    if (!error && data) {
+      return NextResponse.json(data, { status: 200 });
+    }else{
+      return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    };
+  } catch (err) {
+    logger.error("Error while get applications");
+
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+};
